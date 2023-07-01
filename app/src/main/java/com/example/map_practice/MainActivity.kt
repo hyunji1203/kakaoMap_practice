363953a8bbe0e1f84897c8a26ab0882409ace0af
hyunji1203 <hj27720@gmail.com>
2023-07-01 22:10:37 +0900

    feat: 카카오맵 뷰 띄우기

--- app/src/main/java/com/example/map_practice/MainActivity.kt
+++ app/src/main/java/com/example/map_practice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.map_practice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.map_practice.databinding.ActivityMainBinding
+import net.daum.mf.map.api.MapView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapView = MapView(this)
+        binding.mapView.addView(mapView)
     }
 }
+
+
